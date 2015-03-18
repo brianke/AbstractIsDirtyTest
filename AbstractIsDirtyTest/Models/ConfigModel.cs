f85794c994ecfffc9cf5505c92b6f90a7cbfaabe
brianke <brianke@gmail.com>
2015-03-18 11:25:54 -0400

    Initial upload

---
+++ AbstractIsDirtyTest/Models/ConfigModel.cs
@@ -0,0 +1,72 @@
+﻿using SimpleMvvmToolkit;
+using System;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Xml.Serialization;
+
+namespace AbstractIsDirtyTest
+{
+    [XmlRoot("DelphiaConfig")]
+    public class ConfigModel : ModelBase<ConfigModel>
+    {
+        #region Initialization & Cleanup
+
+        // Overriding the GetHashCode prevents the Clone operation from marking an 
+        // object Dirty when it is first cloned
+        public override int GetHashCode()
+        {
+            return GetHashCode(this, "DatabaseList");
+        }
+        private int GetHashCode(object item, params string[] excludeProps)
+        {
+            int hashCode = 0;
+            foreach (var prop in item.GetType().GetProperties())
+            {
+                if (!excludeProps.Contains(prop.Name))
+                {
+                    object propVal = prop.GetValue(item, null);
+                    if (propVal != null)
+                    {
+                        hashCode = hashCode ^ propVal.GetHashCode();
+                    }
+                }
+            }
+            return hashCode;
+        }
+
+        /// <summary>
+        /// Create a configuration object bound to "config.xml" 
+        /// contained in [CommonApplicationData]\[Manufacturer]\[ProductName]
+        /// </summary>
+        public ConfigModel()
+        {
+        }
+
+        #endregion Initialization & Cleanup
+
+
+        #region Properties
+
+        /// <summary>
+        /// DatabaseList 
+        /// </summary>
+        public ObservableCollection<String> DatabaseList
+        {
+            get { return _DatabaseList; }
+            set
+            {
+                if (_DatabaseList != value)
+                {
+                    _DatabaseList = value;
+                    NotifyPropertyChanged(m => m.DatabaseList);
+                }
+            }
+
+        }
+        private ObservableCollection<String> _DatabaseList = new ObservableCollection<String>();
+
+        
+        #endregion Properties
+
+    }
+}
---
+++ AbstractIsDirtyTest/ViewModels/ConfigViewModelDetail.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Linq;
+using System.Windows;
+using System.Threading;
+using System.Collections.ObjectModel;
+
+// Toolkit namespace
+using SimpleMvvmToolkit;
+
+namespace AbstractIsDirtyTest
+{
+    /// <summary>
+    /// This class extends ViewModelDetailBase which implements IEditableDataObject.
+    /// <para>
+    /// Specify type being edited <strong>DetailType</strong> as the second type argument
+    /// and as a parameter to the seccond ctor.
+    /// </para>
+    /// <para>
+    /// Use the <strong>mvvmprop</strong> snippet to add bindable properties to this ViewModel.
+    /// </para>
+    /// </summary>
+    public class ConfigViewModelDetail : ViewModelDetailBase<ConfigViewModelDetail, ConfigModel>
+    {
+        // Overriding the GetHashCode prevents the Clone operation from marking an 
+        // object Dirty when it is first cloned
+        public override int GetHashCode()
+        {
+            return GetHashCode(this, "DatabaseList");
+        }
+        private int GetHashCode(object item, params string[] excludeProps)
+        {
+            int hashCode = 0;
+            foreach (var prop in item.GetType().GetProperties())
+            {
+                if (!excludeProps.Contains(prop.Name))
+                {
+                    object propVal = prop.GetValue(item, null);
+                    if (propVal != null)
+                    {
+                        hashCode = hashCode ^ propVal.GetHashCode();
+                    }
+                }
+            }
+            return hashCode;
+        }
+
+        // Default ctor
+        public ConfigViewModelDetail() 
+        {
+            base.Model = ConfigModel;
+            BeginEdit();        
+        }
+
+        public ConfigModel ConfigModel
+        {
+            get
+            {
+                return _ConfigModel;
+            }
+            set
+            {
+                _ConfigModel = value;
+                NotifyPropertyChanged(m => m.ConfigModel);
+            }
+        }
+        private ConfigModel _ConfigModel = new ConfigModel();
+
+    }
+}
